8190c1453305f9e70cdb0f97430d0890b76b0f1b
arrivabenegustavo <arrivabenegu@gmail.com>
2021-09-27 10:43:10 -0300

    while

--- JavaScript/lista.js
+++ JavaScript/lista.js
@@ -4,8 +4,29 @@ const listaDestinos = new Array(
     `Fortaleza`
 )
 
-console.log(listaDestinos); // mostra a lista
+// console.log(listaDestinos); // mostra a lista
 
-listaDestinos.push(`Bahia`); // Adiciona item na lista
+// listaDestinos.push(`Bahia`); // Adiciona item na lista
 
+// listaDestinos.splice(2, 1); // Exclui um elemento na segunda posição
+
+const idade = 18;
+const estaAcompanhada = false;
+let temPassagemComprada = false;
+
+console.log("Destinos possíveis:");
 console.log(listaDestinos);
+
+const podeComprar = idade >= 18 || estaAcompanhada == true;
+
+const destino = "Fortaleza";
+let contador = 0;
+let destinoExiste = false;
+while(contador < 3){
+    if(listaDestinos[contador] == destino){
+        destinoExiste = true;
+        break;
+    }
+    contador += 1;
+}
+console.log(`Destino existe: ${destinoExiste}`);
