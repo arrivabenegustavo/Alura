c1c378a561593113dd8d606ce60c5d03c95044ed
arrivabenegustavo <arrivabenegu@gmail.com>
2021-09-27 09:20:36 -0300

    lista simples js

---
+++ JavaScript/lista.js
@@ -0,0 +1,11 @@
+const listaDestinos = new Array(
+    `São Paulo`,
+    `Rio de Janeiro`,
+    `Fortaleza`
+)
+
+console.log(listaDestinos); // mostra a lista
+
+listaDestinos.push(`Bahia`); // Adiciona item na lista
+
+console.log(listaDestinos);
