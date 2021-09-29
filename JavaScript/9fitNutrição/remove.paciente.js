911c032111d060c564ff7b8245f4a23fcfbdf4ab
arrivabenegustavo <arrivabenegu@gmail.com>
2021-09-29 19:18:40 -0300

    removndo dados da taberla com settimeout e tagName

--- JavaScript/9fitNutrição/remove.paciente.js
+++ JavaScript/9fitNutrição/remove.paciente.js
@@ -10,7 +10,13 @@
 // Removendo itens da tabela através do pai do evento
 var tabela = document.querySelector("table");
 tabela.addEventListener("dblclick", function(event){
-    event.target.parentNode.remove();
+    if(event.target.tagName == 'TD'){ // Para não apagar os cabeçalhos
+        event.target.parentNode.classList.add("fadeOut");//Busca class que tira as cores dos itens, 
+
+        setTimeout(function(){
+            event.target.parentNode.remove();
+        }, 500); //Faz com que tenha um deley de 0,5 segundos para remover a linha
+}
 })
 //Ou seja, o uso do "target" remove o ALVO, isto é, exatamente o que foi clicado
 // o que chamou o evento, ou seja, apaga apenas a célula onde clicou
