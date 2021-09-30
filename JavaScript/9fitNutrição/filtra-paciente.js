5ebede8dff5bbd2f93b11fb31c6926bd97835e59
arrivabenegustavo <arrivabenegu@gmail.com>
2021-09-30 10:08:50 -0300

    Adicinando filtro com RegExp

--- JavaScript/9fitNutrição/filtra-paciente.js
+++ JavaScript/9fitNutrição/filtra-paciente.js
@@ -1,19 +1,30 @@
 var campoFiltro = document.querySelector("#filtrar-tabela")
 
 campoFiltro.addEventListener("input", function(){
-    //console.log(this.value)// ou campoFiltro.value
+    console.log(this.value)// ou campoFiltro.value
     var pacientes = document.querySelectorAll(".paciente");
-    pacientes.forEach(function(paciente){
-        var tdNome = paciente.querySelector(".info-nome");
-        var nome = tdNome.textContent;
-        console.log(nome)
 
-
-
-    });
-
-
-
-
+    //Se algo for digitado compara e deixa invisível o que for diferente
+    if(this.value.length > 0){
+        for( var i = 0; i < pacientes.length; i++){
+            var paciente = pacientes[i];
+            var tdNome = paciente.querySelector(".info-nome");
+            var nome = tdNome.textContent;
+            //RegExp busca letra por letra
+            var expressao = new RegExp(this.value, "i")// "i"Case insensitive, isto é, procura letras minúsculas e maiúsculas
+            if(expressao.test(nome)){ // se o caracteres na expressão for igual ao nome
+                paciente.classList.remove("invisivel");  
+            }else{
+                paciente.classList.add("invisivel");
+            }
+        }
+    }else{
+        // Se nada for digitado, apaga a classe invisível e mostra a tabela novamente
+        for( var i = 0; i < pacientes.length; i++){              
+            var paciente = pacientes[i];
+            paciente.classList.remove("invisivel")
+        }
+    }
+    
 
 });
